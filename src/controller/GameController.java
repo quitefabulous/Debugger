8b55a9ffd240a7239c33fa96ec5692dae48e4856
Lena Schmid <lena-2.schmid@uni-ulm.de>
2018-01-31 12:47:11 +0100

    Hallu

---
+++ src/controller/GameController.java
@@ -0,0 +1,81 @@
+package controller;
+
+import application.*;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import model.*;
+import view.GameView;
+import javafx.scene.input.*;
+
+public class GameController {
+
+    private GameModel gameModel;
+    private GameView gameView;
+    private Scene scene;
+    private boolean doAccelerate = false;
+    
+    public GameController(GameModel gameModel, GameView gameView) {
+        this.gameView = gameView;
+        this.gameModel = gameModel;
+        this.scene = gameView.getScene();
+        //Set up keylistener
+        setUpInputHandler();
+    }
+    
+
+
+    public void updateContinuously(double timeDifferenceInSeconds) {
+    	Car newCar = gameModel.getCar();
+    	if(doAccelerate == true) {
+    		newCar.calcAirDensity(timeDifferenceInSeconds);
+    		newCar.calcFriction();
+    		newCar.calcSpeed(timeDifferenceInSeconds);
+    	}
+ 	}
+    
+    
+    private void setUpInputHandler() {
+        /*
+         * Useful actions:
+         * setOnKeyPressed, setOnKeyReleased
+         */
+    
+    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		@Override
+		public void handle(KeyEvent event) {
+			Car newCar = gameModel.getCar();
+			String code = event.getCode().toString();
+			
+			
+			// It only reacts if the game is running.
+			if (gameModel.getGameState() == GameState.RUNNING) {
+				switch (code) {
+				case "UP":
+					doAccelerate = true;
+					newCar.straightAcc();
+					break;
+				case "LEFT":
+					doAccelerate = true;
+					newCar.leftAcc();
+					break;
+				case "RIGHT":
+					doAccelerate = true;
+					newCar.rightAcc();
+				case "DOWN" :
+					doAccelerate = true;
+					newCar.backAcc();
+				default:
+					break;
+				}
+			}
+		}
+    });
+    
+    scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+    	@Override
+		public void handle(KeyEvent event) {
+			doAccelerate = false;
+    	}
+    });
+}
+}
---
+++ src/model/Subsoil.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Subsoil {
+public double FRICTION_VARIABLE;
+
+	public boolean color() {
+	boolean colorBlack = true;
+	return colorBlack;
+	}
+
+}
+
